4d5a1e7d014bb7fb28e4b0c0a519ba2653ac8293
leonardonogueira07 <leojunio007@gmail.com>
2024-11-27 23:53:57 -0300

    Alteraçao do caminho do logotipo

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,10 +3,7 @@ import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 
 const Search = styled('div')(({ theme }) => ({
@@ -57,7 +54,7 @@ export default function NavBar({pokemonFilter}) {
       <AppBar position="static" sx={{ background: "black" }}>
         <Toolbar>
           <Box display="flex" justifyContent="space-between" width="100%">
-            <Box component="img" src='/assets/pokemon-logo.png' height="3em" />
+            <Box component="img" src={`${process.env.PUBLIC_URL}/assets/pokemon-logo.png`} height="3em" />
             <Search onChange={(e) => pokemonFilter(e.target.value)}>
               <SearchIconWrapper>
                 <SearchIcon />
